6762fa11f582b555b81cbea23f522bfcbd121c4e
Aiden Bohlander <aiden.bohlander@gmail.com>
2026-09-22 23:33:35 -0700

    balance times again

--- client/client.py
+++ client/client.py
@@ -1,6 +1,7 @@
 import os
 import time
 import requests
+from datetime import timedelta
 from ykman.device import list_all_devices
 from yubikit.core.smartcard import SmartCardConnection
 from yubikit.oath import OathSession
@@ -8,7 +9,7 @@ from yubikit.oath import OathSession
 
 SERVER_HOSTNAME = os.environ["SERVER_HOSTNAME"]
 OATH_CREDENTIAL_ID = b"d-point:d-point"
-UPDATE_INTERVAL_SECONDS = 60
+UPDATE_INTERVAL = timedelta(minutes=5)
 
 
 device, info = list_all_devices([SmartCardConnection])[0]
@@ -20,4 +21,4 @@ with device.open_connection(SmartCardConnection) as connection:
             response = requests.get(f"http://{SERVER_HOSTNAME}/nonce")
             assert response.status_code == 200
             f.write(oath.calculate(OATH_CREDENTIAL_ID, bytes.fromhex(response.text)).hex())
-        time.sleep(UPDATE_INTERVAL_SECONDS)
+        time.sleep(UPDATE_INTERVAL.total_seconds())
